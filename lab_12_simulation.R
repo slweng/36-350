65ce958dc7cd3a5069d6fcc0cde4fa924797acb9
Shawn Weng <slweng@andrew.cmu.edu>
2018-04-17 12:57:45 -0400

    added comments for `generate_data(n,p)`

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,7 +1,9 @@
 generate_data = function(n, p) {
+  # create an n x p matrix of random draws from the standard normal distribution
   covariates = rnorm(n*p)
   covariates = matrix(covariates, nrow = n, ncol = p)
   
+  # draw n responses from the standard normal distribution
   responses = rnorm(n)
   
   return(list(covariates = covariates, responses = responses))
