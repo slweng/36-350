d3fc463fad91112353aa4e1cdc4cd5584b0ab78f
Shawn Weng <slweng@andrew.cmu.edu>
2018-04-17 14:08:29 -0400

    created function `model_select(covariates, responses, cutoff)`

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,3 +8,34 @@ generate_data = function(n, p) {
   
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select = function(covariates, responses, cutoff) {
+  # runs a linear regression of responses ~ covariates
+  lm = lm(responses ~ covariates)
+  
+  # save summary data
+  lm.coefficients = summary(lm)$coefficients
+
+  # p-values
+  lm.pvalues = lm.coefficients[,"Pr(>|t|)"]
+  
+  # find significant covariates
+  significant.cov = which(lm.pvalues <= cutoff) - 1
+  
+  # if no covariates are significant, return an empty vector
+  if (length(significant.cov) == 0) return(c())
+  # if only intercept is significant, return an empty vector
+  else if ((length(significant.cov) == 1 & significant.cov == 0)) return(c())
+  
+  # otherwise...if intercept is significant, keep names
+  if (significant.cov[1] == 0) sig.names = names(significant.cov)
+  else sig.names = c("(Intercept)", names(significant.cov)) # add intercept name if it doesn't exist
+    
+  # run a linear regression using only significant covariates
+  significant.cov = significant.cov[which(significant.cov != 0)] # only keep covariates, not intercept
+  lm = lm(responses ~ covariates[,significant.cov])
+  p.values = summary(lm)$coefficients[,"Pr(>|t|)"]
+  names(p.values) = sig.names
+  
+  return(p.values)
+}
